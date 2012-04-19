2a3d8b2c5cccf12b9985ca4cd8d5699a46c23dec
Mateusz Haligowski <mhaligowski@googlemail.com>
2012-04-19 19:40:44 +0200

    Issue #3: create permission for creating booklist

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from userena.models import UserenaBaseProfile
 from django.db.models.signals import post_save
 from tastypie.models import create_api_key
+
+import logging
+logger = logging.getLogger(__name__)
 
 class UserProfile(UserenaBaseProfile):
     # user reference
@@ -14,8 +17,13 @@ def create_user_profile(sender, instance, created, **kwargs):
     """
     Create user profie and set the permissions
     """
-    if created:
+    logger.debug(instance)
+    if created and instance.pk >= 0:
         UserProfile.objects.create(user=instance)
+        
+        # get default group, but not for anonymous
+        default_group = Group.objects.get(pk = 0)
+        instance.groups.add(default_group)
         
 post_save.connect(create_user_profile, sender=User)
 
